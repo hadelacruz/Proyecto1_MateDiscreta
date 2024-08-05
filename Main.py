1fa81f28842a81294a649e2a451c9e6280c6a8f4
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-08-05 09:27:30 -0600

    Se agregó la estructura inicial del programa

---
+++ Main.py
@@ -0,0 +1,51 @@
+def menu():
+    while True:
+        print("\n-------------OPERACIONES-------------")
+        print("1. A ∪ C")
+        print("2. A ∩ C")
+        print("3. A \\ B")
+        print("4. A̅")
+        print("5. (A ∪ B) ∩ C")
+        print("6. fun(E)")
+        print("7. D̅")
+        print("8. D \\ U")
+        print("9. fun(A × B)")
+        print("10. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == '1':{
+
+        }
+        elif opcion == '2':{
+
+        }
+        elif opcion == '3':{
+
+        }
+        elif opcion == '4':{
+
+        }           
+        elif opcion == '5':{
+
+        }
+        elif opcion == '6':{
+
+        }
+        elif opcion == '7':{
+
+        }
+        elif opcion == '8':{
+
+        }
+        elif opcion == '9':{
+
+        }
+        elif opcion == '10':
+            break   
+
+        else:    
+            print("\nSelecciona una opción válida")
+
+if __name__ == "__main__":
+    menu()
